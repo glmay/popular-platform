67b9c4d8b3a0e0c01467e0bde29d7af65e1f739f
zzh0211 <zzh0211@live.com>
2019-04-20 11:11:49 +0800

    remove eslint

--- src/App.vue
+++ src/App.vue
@@ -3,44 +3,51 @@
 		<div id="nav">
 			<router-link to="/">Home</router-link> |
 			<router-link to="/about">About</router-link>
-
-			<h2>{{ name }}</h2>
 		</div>
 		<router-view />
 	</div>
 </template>
 
 <script>
-import axios from 'axios'
-import $ from 'jquery'
-import { METHODS } from 'http';
+	import axios from 'axios'
+	import qs from 'querystring'
+	import $ from 'jquery'
+	export default {
+		created() {
+			axios.defaults.headers.Authorization =
+				'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyMjAxMzAyNiwidXNlcm5hbWUiOiJkaXNhbmZhbmc0IiwiZXhwIjoxNTU1NzU2MTI3LCJvcmlnX2lhdCI6MTU1NTcyNzMyN30.xEKNANCY2rf5XDZr7zaZcOqSC30e3E0x9bxPeKvejmo'
+			axios.get('/v1/level/'
+				/* , {
+							headers: {
+								Authorization: 'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyMjAxMzAyNiwidXNlcm5hbWUiOiJkaXNhbmZhbmc0IiwiZXhwIjoxNTU1NzU0NTk5LCJvcmlnX2lhdCI6MTU1NTcyNTc5OX0.MCx1R5W_HNxN7yOH82aCDOh-zefT1WnhIsWe07unaQ0'
+							}
+						} */
+			);
 
-export default {
-	data(){
-		return {
-			name: 123
-		}
-	},
-	created(){
-		window.axios = axios;
-		window.$ = $;
+			axios.post('/v1/login/', qs.stringify({
+				username: 'disanfang4',
+				password: '123456xyz',
+				code: '11',
+			}));
 
-		this.init();
-	},
-	methods: {
-		init(){
-			axios.get('/api/data.json');
-
-			// $.get('/api/data.json');
+			/* $.ajax({
+				url: '/api/login/',
+				data: {
+					username: 'disanfang4',
+					password: '123456xyz',
+					code: '11',
+				},
+				type: 'post',
+				dataType: 'json',
+			}); */
 		}
 	}
-}
 </script>
 
 
 <style lang="scss">
 	#app {
-		font-family: "Avenir", Helvetica, Arial, sans-serif;
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
 		-webkit-font-smoothing: antialiased;
 		-moz-osx-font-smoothing: grayscale;
 		text-align: center;
@@ -59,5 +66,4 @@ export default {
 			}
 		}
 	}
-
 </style>
